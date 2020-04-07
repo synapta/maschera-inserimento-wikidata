b04c2c529a9534da2eb1791daeb61d35d0686fd4
alemela <alessiom92@gmail.com>
2020-04-07 12:42:43 +0200

    map focus after improved address

--- app/js/comuni.js
+++ app/js/comuni.js
@@ -7,6 +7,17 @@ $('.ui.search')
       results : 'items',
       title   : 'title'
     },
-    minCharacters : 3
+    minCharacters : 3,
+    onSelect: function(res, resp) {
+        $.ajax({
+            type: "GET",
+            url: `https://nominatim.openstreetmap.org/search.php?q=${res.title}, Italia&format=json`,
+            success: function(data){
+                console.log(data[0].lat, data[0].lon);
+                mymap.panTo(new L.LatLng(data[0].lat, data[0].lon));
+                mymap.setZoom(10);
+            }
+        });
+    }
   })
 ;
--- app/js/maschera.js
+++ app/js/maschera.js
@@ -1,6 +1,20 @@
 let mainObject = {};
 
 document.getElementById("sendMaschera").addEventListener("click", function () {
+  let label = $("input[name=label]").val();
+  if (label) {
+      mainObject.label = label;
+  }
+
+  let descrizione = $("input[name=descrizione]").val();
+  if (descrizione) {
+      mainObject.descrizione = descrizione;
+  }
+
+  let tipologia = $("input[name=tipologia]").val();
+  if (tipologia) {
+      mainObject.P31 = tipologia;
+  }
 
   let comune = $('.ui.search').search('get result', $("input[name=comune]").val());
   if (comune) {
@@ -11,6 +25,16 @@ document.getElementById("sendMaschera").addEventListener("click", function () {
   let long = $("input[name=long]").val();
   if (lat && long) {
       mainObject.P625 = `Point(${lat}, ${long})`
+  }
+
+  let indirizzo = $("input[name=indirizzo]").val();
+  if (indirizzo) {
+      mainObject.P6375 = indirizzo;
+  }
+
+  let website = $("input[name=sito-web]").val();
+  if (website) {
+      mainObject.P856 = website;
   }
 
   console.log(mainObject);
